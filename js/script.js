aab8e39ac6b730ea10e6893e84b9b614e2b7f54b
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-10-09 21:08:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing" ,{
+    strings:["","Software Developer", "Programmer", "Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
